27b3ee95f44c2703ecd3aa5b1cda6e52f250ab11
1 <1>
2026-08-15 15:26:21 +0300

    upd init

---
+++ src/tools/__init__.py
@@ -0,0 +1,11 @@
+from .calc import CalcTool
+from .crypto import CryptoTool
+from .http import HTTPTool
+from .news import NewsTool
+from .weather import WeatherTool
+
+
+__all__ = [
+    "CalcTool", "CryptoTool", "HTTPTool", 
+    "NewsTool", "WeatherTool"
+]
